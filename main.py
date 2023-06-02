ba08bede8e41adbc61e86a557d995c6962c1af7f
boya-madhu <madhu@de-haze.com>
2023-06-02 23:19:29 +0530

    Add Python API

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print("Hello World!!!")
+
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route("/")
+def APIS():
+    return "This is python APIS"
+
+if __name__=='__main__':
+    app.run()
